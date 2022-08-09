f66aa0763739fc0bbc19d4d9c652a4901a0a8390
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-09 17:58:35 +0200

    feat: initializing translations files

--- src/i18next.js
+++ src/i18next.js
@@ -1,35 +1,54 @@
 import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide 
-// for passing in lng and translations on init
+const fallbackLng = ['en'];
+const availableLanguages = ['en', 'fr', 'chi'];
 
-const Languages = ['en','fr','chi']
+
+const options = {
+    // order and from where user language should be detected
+    order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+
+    // keys or params to lookup language from
+    lookupQuerystring: 'lng',
+    lookupCookie: 'i18next',
+    lookupLocalStorage: 'i18nextLng',
+    lookupFromPathIndex: 0,
+    lookupFromSubdomainIndex: 0,
+
+    // cache user language on
+    caches: ['localStorage', 'cookie'],
+    excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+    // optional expire and domain for set cookie
+    cookieMinutes: 10,
+    cookieDomain: 'myDomain',
+
+    // optional htmlTag with lang attribute, the default is:
+    htmlTag: document.documentElement,
+
+    // only detect languages that are in the whitelist
+    checkWhitelist: true
+}
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    whitelist: Languages,
+    .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
 
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    }
-  });
+    .use(LanguageDetector) // detect user language
 
+    .use(initReactI18next) // pass the i18n instance to react-i18next.
+
+    .init({
+        fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
+        debug: true,
+        whitelist: availableLanguages,
+        detection: options,
+
+        interpolation: {
+            escapeValue: false
+        },
+    });
 
 export default i18n;

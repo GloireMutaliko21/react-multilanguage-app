130aa3a66567d20feca718cc67d85d559a34a202
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-09 17:58:35 +0200

    feat: finalizing

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Installation de la librairie i18next
+
     Utiliser la commande `npm install react-i18next i18next --save`
     Cette commande va vous telecharger le paquet
+    Vous aurez besoin d'installer certains paquets manuellement s'ils génèrent des erreurs
 
+# Initialisez les fichiers de configuration de traduction
+
+    De préférence, créez un fichier pour chaque langue que vous utiliserez
+    Ces fichiers doivent être contenus dans des dossiers, ceux-ci contenus dans le dossier `public`
+
+    ## Soyez vigilants sur la nomination de vos dossiers :
+        Ils doivent avoir les mêmes noms que votre tableau de langages dans `i18next.js`
--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import { useTranslation } from 'react-i18next'
+import i18next from 'i18next';
 
 function App() {
-
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   function handleClick(lang) {
-    i18n.changeLanguage(lang)
+    i18next.changeLanguage(lang)
   }
 
   return (
     <div className="App">
-
       <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
         <button onClick={() => handleClick('en')}>
           English
@@ -27,12 +25,9 @@ function App() {
         </button>
       </nav>
       <header>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {t('To get started, edit src/App.js and save to reload.')}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3>
         </p>
       </header>
     </div>
